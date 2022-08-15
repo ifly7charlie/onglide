3c8d2c61d0636e9ab61f1714db9fed1f06214d11
ifly7charlie <melissa-github@littlebluecar.co.uk>
2022-08-15 07:44:11 +0200

    Replay hack

--- pages/api/contest.js
+++ pages/api/contest.js
@@ -20,9 +20,15 @@ export default async function competitionHandler(req, res) {
         return;
     }
 
-    const classes = await query(escape`
+    const classes = await query(
+        process.env.REPLAY
+            ? escape`
+         SELECT c.class, c.classname, c.description, todcode(from_unixtime(${process.env.REPLAY})) datecode, cs.status
+           FROM classes c, compstatus cs where c.class=cs.class ORDER BY c.class`
+            : escape`
          SELECT c.class, c.classname, c.description, cs.datecode, cs.status
-           FROM classes c, compstatus cs where c.class=cs.class ORDER BY c.class`);
+           FROM classes c, compstatus cs where c.class=cs.class ORDER BY c.class`
+    );
 
     // How long should it be cached - 5 minutes is ok
     res.setHeader('Cache-Control', 'public, s-maxage=300, stale-while-revalidate=300');
