b08d3cf9fde48c4d39b0eea5b1a6b90b05480c4a
ifly7charlie <melissa-github@littlebluecar.co.uk>
2022-08-19 10:22:01 +0200

    Layout

--- lib/react/pilotlist.tsx
+++ lib/react/pilotlist.tsx
@@ -160,7 +160,8 @@ export function Details({units, pilot, score, vario, tz}: {score: PilotScore | n
                     {speed}
                     {speed ? ',' : ''}
                     {distance}
-                    <Optional b=", Glide ratio to Finish" v={score.actual?.grRemaining} e=":1" />
+                    {score.handicapped?.grRemaining ? <br /> : ', '}
+                    <Optional b="Glide ratio to Finish" v={score.actual?.grRemaining} e=":1" />
                     <Optional b=", HCap Ratio" v={score.handicapped?.grRemaining} e=":1" />
                     <FlightLegs score={score} tz={tz} units={units} />
                 </div>
