6795d9dd0a972fc444fa6c45c09d61d8e38eb10e
ifly7charlie <melissa-github@littlebluecar.co.uk>
2023-08-05 12:43:19 +0100

    typescript

--- lib/react/displayunits.ts
+++ lib/react/displayunits.ts
@@ -1,15 +1,15 @@
-export function convertClimb(c: number, units: number): [number, string] {
+export function convertClimb(c: number, units: boolean): [number, string] {
     return [Math.round(c * (units ? 19.43844 : 10)) / 10, units ? 'kt' : 'm/s'];
 }
 
-export function convertHeight(c: number, units: number): [number, string] {
+export function convertHeight(c: number, units: number | boolean): [number, string] {
     return [Math.round(c * (units ? 3.28084 : 1)), units ? 'ft' : 'm'];
 }
 
-export function displayClimb(c: number, units: number): string {
+export function displayClimb(c: number, units: boolean): string {
     return convertClimb(c, units).join('');
 }
 
-export function displayHeight(c, units): string {
+export function displayHeight(c: number, units: number | boolean): string {
     return convertHeight(c, units).join('');
 }
