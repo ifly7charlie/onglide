73206b520231120306a697f20b7c67429a975cbd
ifly7charlie <melissa-github@littlebluecar.co.uk>
2023-08-22 18:39:39 +0200

    Add keys to flight legs table

--- lib/react/flightLegs.tsx
+++ lib/react/flightLegs.tsx
@@ -55,7 +55,7 @@ export function FlightLegs({score, units, tz}) {
         }
         if (l.maxPossible && l.minPossible && Math.trunc(l.minPossible) != Math.round(l.maxPossible)) {
             return (
-                <td style={{fontSize: 'small'}}>
+                <td style={{fontSize: 'small'}} key="mp_dr">
                     {Math.trunc(l.minPossible)}-{Math.round(l.maxPossible)}
                     <br />
                     {l.distanceRemaining}
@@ -64,7 +64,7 @@ export function FlightLegs({score, units, tz}) {
         }
         if (l.maxPossible) {
             return (
-                <td style={{fontSize: 'small'}}>
+                <td style={{fontSize: 'small'}} key="mp">
                     {l.maxPossible}
                     <br />
                     {l.distanceRemaining}
@@ -72,7 +72,7 @@ export function FlightLegs({score, units, tz}) {
             );
         }
         if (l.distanceRemaining > 0) {
-            return <td>{l.distanceRemaining}</td>;
+            return <td key="dr">{l.distanceRemaining}</td>;
         }
         return null;
     };
@@ -80,7 +80,7 @@ export function FlightLegs({score, units, tz}) {
         const l = accessor(x);
         if (l && l.maxPossible) {
             return (
-                <td style={{fontSize: 'small'}}>
+                <td style={{fontSize: 'small'}} key="legend">
                     Possible
                     <br />
                     Shortest
@@ -88,7 +88,7 @@ export function FlightLegs({score, units, tz}) {
             );
         }
         if (l.distanceRemaining > 0) {
-            return <td>Shortest</td>;
+            return <td key="legend">Shortest</td>;
         }
         return null;
     };
@@ -130,7 +130,7 @@ export function FlightLegs({score, units, tz}) {
                         <tr>
                             <td>&nbsp;</td>
                             {_map(actualLegs, (x) => (
-                                <td>
+                                <td key={x.legno.toString()}>
                                     Leg {x.legno} {legIcon(x)}
                                 </td>
                             ))}
@@ -139,28 +139,28 @@ export function FlightLegs({score, units, tz}) {
                     <tbody>
                         <tr style={{fontSize: 'small'}}>
                             <td>Leg Start Altitude</td>
-                            {_map(actualLegs, (x) => (x?.alt > 0 ? <td>{displayHeight(x?.alt, units)}</td> : null))}
+                            {_map(actualLegs, (x) => (x?.alt > 0 ? <td key={x.legno.toString()}>{displayHeight(x?.alt, units)}</td> : null))}
                         </tr>
                         <tr>
                             <td>Leg Start</td>
-                            {_map(actualLegs, (x) => (x.time ? <td>{OptionalTimeHHMM('', x.time as Epoch, tz)}</td> : null))}
+                            {_map(actualLegs, (x) => (x.time ? <td key={x.legno.toString()}>{OptionalTimeHHMM('', x.time as Epoch, tz)}</td> : null))}
                         </tr>
                         <tr style={{fontSize: 'small'}}>
                             <td>Leg Duration</td>
-                            {_map(actualLegs, (x) => (x.duration ? <td>{OptionalDurationHHMM('+', x.duration as Epoch)}</td> : null))}
+                            {_map(actualLegs, (x) => (x.duration ? <td key={x.legno.toString()}>{OptionalDurationHHMM('+', x.duration as Epoch)}</td> : null))}
                         </tr>
                         {!viewOptions.task ? (
                             <>
                                 <tr>
                                     <td>Leg Distance</td>
                                     {_map(actualLegs, (x) => (
-                                        <td>{accessor(x)?.distance || ''}</td>
+                                        <td key={x.legno.toString()}>{accessor(x)?.distance || ''}</td>
                                     ))}
                                 </tr>
                                 <tr>
                                     <td>Leg Speed</td>
                                     {_map(actualLegs, (x) => (
-                                        <td>{accessor(x)?.legSpeed}</td>
+                                        <td key={x.legno.toString()}>{accessor(x)?.legSpeed}</td>
                                     ))}
                                 </tr>
                                 {!score.utcFinish && (
@@ -176,13 +176,13 @@ export function FlightLegs({score, units, tz}) {
                                 <tr>
                                     <td>Task Speed</td>
                                     {_map(actualLegs, (x) => (
-                                        <td>{accessor(x)?.taskSpeed || ''}</td>
+                                        <td key={x.legno.toString()}>{accessor(x)?.taskSpeed || ''}</td>
                                     ))}
                                 </tr>
                                 <tr>
                                     <td>Task Distance</td>
                                     {_map(actualLegs, (x) => (
-                                        <td>{accessor(x)?.taskDistance || ''}</td>
+                                        <td key={x.legno.toString()}>{accessor(x)?.taskDistance || ''}</td>
                                     ))}
                                 </tr>
                                 {!score.utcFinish && (
