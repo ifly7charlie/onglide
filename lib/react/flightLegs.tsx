5e9a19b1e87196f62de588c678e93eb0fdc02939
ifly7charlie <melissa-github@littlebluecar.co.uk>
2023-08-28 08:35:19 +0200

    Key a td

--- lib/react/flightLegs.tsx
+++ lib/react/flightLegs.tsx
@@ -167,7 +167,7 @@ export const FlightLegs = memo(function FlightLegs({score, units, tz}: {score: a
                                 {!score.utcFinish && (
                                     <tr>
                                         {distanceRemainingLegend(score)}
-                                        {_map(actualLegs, (x) => (x.legno >= score.currentLeg ? distanceRemaining(x) : <td></td>))}
+                                        {_map(actualLegs, (x) => (x.legno >= score.currentLeg ? distanceRemaining(x) : <td key={x.legno.toString()}></td>))}
                                     </tr>
                                 )}
                             </>
