f4d8da6073aa4650efabb871d6a16a7cfc5be9d4
ifly7charlie <melissa-github@littlebluecar.co.uk>
2024-06-23 15:35:40 +0200

    don't load map till options loaded

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,7 +132,7 @@ export default function CombinePage(props) {
 
     //
     // And display in progress until they are loaded
-    if (isLoading)
+    if (isLoading || !props.options)
         return (
             <div className="loading">
                 <div className="loadinginner" />
@@ -168,7 +168,20 @@ export default function CombinePage(props) {
             </Head>
             <Menu comp={comp} vc={className} setSelectedPilot={setSelectedCompno} measureFeatures={measureFeatures} options={props.options} setOptions={props.setOptions} />
             <div className="resizingContainer">
-                <OgnFeed vc={className as ClassName} tz={props.tz} datecode={selectedClass ? selectedClass.datecode : '07C'} selectedCompno={selectedCompno} setSelectedCompno={setSelectedCompno} viewport={viewport} setViewport={setViewport} options={props.options} setOptions={props.setOptions} measureFeatures={measureFeatures} handicapped={selectedClass?.handicapped == 'Y'} notes={selectedClass?.notes} />
+                <OgnFeed
+                    vc={className as ClassName} //
+                    tz={props.tz}
+                    datecode={selectedClass ? selectedClass.datecode : '07C'}
+                    selectedCompno={selectedCompno}
+                    setSelectedCompno={setSelectedCompno}
+                    viewport={viewport}
+                    setViewport={setViewport}
+                    options={props.options}
+                    setOptions={props.setOptions}
+                    measureFeatures={measureFeatures}
+                    handicapped={selectedClass?.handicapped == 'Y'}
+                    notes={selectedClass?.notes}
+                />
             </div>
         </>
     );
