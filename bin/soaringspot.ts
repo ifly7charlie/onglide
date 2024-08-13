7a7d59dcd2fd19347e2fdda7ee1e05a8a985a3d5
Melissa Jenkins <melissa-github@littlebluecar.co.uk>
2024-08-13 10:22:02 +0000

    reduce load on soaringspot

--- bin/soaringspot.ts
+++ bin/soaringspot.ts
@@ -169,6 +169,10 @@ async function soaringSpot(deep = false) {
     // links in this code are HTTP because that is how they are returned in the JSON
     // HOWEVER! All fetches will be https because the enumeration links are all https
     const contests = (await sendSoaringSpotRequest('https://api.soaringspot.com/v1/', keys)) as any;
+    if( ! contests ) {
+        console.log('unable to fetch contest');
+        return;
+    }
 
     // loop through all, there probably is only one but API spec implies more than one.
     await contests._embedded['http://api.soaringspot.com/rel/contests'].forEach(async function (contest) {
@@ -244,6 +248,10 @@ async function update_pilots(class_url, classid, classname, keys) {
 
     // Fetch the list of pilots
     const results = await sendSoaringSpotRequest(class_url + '/contestants', keys);
+    if( ! results ) {
+        console.log(classid, 'unable to fetch contestants');
+        return;
+    }
 
     // Start a transaction for updating pilots
     let t = mysql_db.transaction();
@@ -380,12 +388,12 @@ async function download_picture(compno, classid, mysql, context) {
 // for a given class update all the tasks
 async function process_class_tasks(class_url, classid, classname, keys) {
     const tasks = await sendSoaringSpotRequest(class_url + '/tasks', keys);
-    if ('code' in tasks || !('_embedded' in tasks)) {
-        console.log(`${classname}: unable to fetch tasks ${tasks.message}`);
+    if (!tasks || 'code' in tasks || !('_embedded' in tasks)) {
+        console.log(`${classname}: unable to fetch tasks ${tasks?.message}`);
         return 0;
     }
     let dates: string[] = [];
-    for (const day of tasks._embedded['http://api.soaringspot.com/rel/tasks']) {
+    for (const day of tasks._embedded['http://api.soaringspot.com/rel/tasks'].sort((a,b)=>-a.task_date.localeCompare(b.task_date)).slice(0,1)) {
         dates.push(day.task_date);
 
         // Download the task and prep pilotresult table
@@ -399,8 +407,8 @@ async function process_class_tasks(class_url, classid, classname, keys) {
 // for a given class update all the results
 async function process_class_results(class_url, classid, classname, keys) {
     const results = await sendSoaringSpotRequest(class_url + '/results', keys);
-    if ('code' in results || !('_embedded' in results)) {
-        console.log(`${classname}: no results: ${results.message}`);
+    if (!results || 'code' in results || !('_embedded' in results)) {
+        console.log(`${classname}: no results: ${results?.message}`);
         return 0;
     }
 
@@ -933,7 +941,14 @@ function soaringSpotAuthHeaders(keys) {
 // Fetch values from the soaringpot api
 //
 async function sendSoaringSpotRequest(url, keys): Promise<any> {
-    return fetch(url, soaringSpotAuthHeaders(keys)).then((res) => res.json());
+    return fetch(url, soaringSpotAuthHeaders(keys)).then(async (res) => {
+        if (!res.ok) {
+            console.log( 'Error with soaring spot', url, res.status);
+        }
+        const result = await res.text();
+        console.log(result);
+        return JSON.parse(result);
+    }).catch((e) => { console.error(e); return null});
 }
 
 // Get rid of the T at the front...
