dfe5612a56ad90b283727837eb2b7a9f743c6710
ifly7charlie <melissa-github@littlebluecar.co.uk>
2025-08-12 00:20:53 +0200

    Update distancehandicap.ts
    calculation appears to add the 0.5*2 for TPS before calculating, except it has an extra 0.5?? hard to tell as seems to be confusion between vicenty and haversine usage. FAI rules for internations us 6371 but official observers state vicenty. this explains some of the difference in calculations between onglide & seeyou

--- lib/flightprocessing/distancehandicap.ts
+++ lib/flightprocessing/distancehandicap.ts
@@ -11,36 +11,38 @@ export function adjustDistanceHandicapTask(task: Task, handicap: number | undefi
     if (!handicap) {
         return task;
     }
-
     // Make a new array for it
     var newTask = _clonedeep(task);
 
-    const eligibletps = task.legs.filter((leg) => leg.type == 'sector' && leg.r2 >= 0.5 && leg.a2 == 180)?.length ?? 0;
+    const eligibletps = task.legs.filter((leg) => leg.type == 'sector' && leg.r2 >= 0.5 && leg.a1 == 90)?.length ?? 0;
     if (!eligibletps) {
         console.error('no eligible legs found in task', task);
         return task;
     }
+
+    const actualDistance = task.details.distance + eligibletps; // add 1km for each eligible turnpoint
+
     // reduction amount (%ish)
-    var maxhtaskLength = task.details.distance * (100 / task.rules.maxHandicap);
+    var maxhtaskLength = actualDistance * (100 / task.rules.maxHandicap);
     const targetActualDistance = maxhtaskLength * (handicap / 100);
 
-    var sectorSize = 0.5 + (task.details.distance - targetActualDistance) / (2 * eligibletps);
+    var sectorSize = 0.5 + (actualDistance - targetActualDistance) / (2 * eligibletps);
 
     // how far we need to move the radius in to achieve this reduction
 
-    console.log(
-        `dh ${task.details.class}: td: ${task.details.distance} handicaps: ${handicap}/${task.rules.maxHandicap}: maxhtaskLength:${maxhtaskLength}, targetActualDistance: ${targetActualDistance}, sectorSize: ${sectorSize}`
-    );
+    //    console.log(
+    //        `dh ${task.details.class}: td: ${task.details.distance}=>${actualDistance} handicaps: ${handicap}/${task.rules.maxHandicap}: maxhtaskLength:${maxhtaskLength}, targetActualDistance: ${targetActualDistance}, sectorSize: ${sectorSize}`
+    //    );
 
     // Now copy over the points reducing all symmetric
     newTask.legs.slice(1, newTask.legs.length - 1).forEach((leg: TaskLeg) => {
-        if (leg.type == 'sector' && leg.direction == 'symmetrical' && leg.r2 >= 0.5 && leg.a2 == 180) {
+        if (leg.type == 'sector' && leg.direction == 'symmetrical' && leg.r2 >= 0.5 && leg.a1 == 90) {
             leg.r2 = sectorSize as DistanceKM;
         }
     });
 
     // Calculate the sectors for the adjusted task
     calculateTask(newTask);
-    console.log(`dh: adjust ${handicap}: actual distance: ${newTask.details.distance}, hcap distance: ${newTask.details.distance * (handicap / 100)}`);
+    //    console.log(`dh: adjust ${handicap}: actual distance: ${newTask.details.distance}, hcap distance: ${newTask.details.distance * (handicap / 100)}`);
     return newTask;
 }
