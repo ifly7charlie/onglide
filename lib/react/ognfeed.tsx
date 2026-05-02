0dad42103b363cbaab05594df17f5e9d2b718714
ifly7charlie <melissa-github@littlebluecar.co.uk>
2026-05-02 12:34:29 +0100

    local trimmed logo

--- lib/react/ognfeed.tsx
+++ lib/react/ognfeed.tsx
@@ -66,7 +66,7 @@ const MApp = dynamic(() => import('./deckgl').then((mod) => mod), {
     loading: () => (
         <div style={{width: '100vw', marginTop: '20vh', position: 'absolute'}}>
             <div style={{display: 'block', margin: 'auto', width: '100px'}}>
-                <img width="100" height="100" src="https://ognproject.wdfiles.com/local--files/logos/ogn-logo-150x150.png" alt="OGN Network" title="OGN Network" />
+                <img width="100" height="100" src="/ognlogo.png" alt="OGN Network" title="OGN Network" />
             </div>
         </div>
     )
