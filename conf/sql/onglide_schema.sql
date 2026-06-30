895d19c9685d01cb8c4645770512d92ee7174dcc
ifly7charlie <melissa-github@littlebluecar.co.uk>
2026-06-30 17:08:10 +0200

    Update onglide_schema.sql

--- conf/sql/onglide_schema.sql
+++ conf/sql/onglide_schema.sql
@@ -384,7 +384,7 @@ CREATE TABLE `trackerhistory` (
   `flarmtype` char(3) DEFAULT NULL,
   `greg` char(12) DEFAULT NULL,
   `launchtime` time DEFAULT NULL,
-  `method` enum('none','startline','pilot','ognddb','igcfile','tltimes','robocontrol','grandprix','soaringspot','ogn-blocked','flarmnet-blocked','ddb-blocked','startmatch','evidence','startmatch-swap','uncorrelated') DEFAULT 'none',
+  `method` enum('none','startline','pilot','ognddb','igcfile','tltimes','robocontrol','grandprix','soaringspot','ogn-blocked','displaced','flarmnet-blocked','ddb-blocked','startmatch','evidence','startmatch-swap','uncorrelated') DEFAULT 'none',
   `class` char(15) DEFAULT NULL,
   `datecode` char(3) DEFAULT NULL,
   `delta_start` smallint DEFAULT NULL,
